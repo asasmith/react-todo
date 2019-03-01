8ea6e132ba00514bd478c1666886e81cc1ab3b78
Asa Smith <asa.smith@berndtgroup.net>
2019-03-01 17:55:41 -0500

    :tada:

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => {
+  return <h1>Hello World!</h1>
+}
+
+ReactDOM.render(<Index />, document.getElementById('index'))
